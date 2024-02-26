78dbdb06383522c8ad8be708e447b552bd75a9a8
Karsten Dambekalns <karsten@dambekalns.de>
2024-02-26 21:11:44 +0100

    Remove session_save_path() calls

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-session_save_path(realpath(dirname($_SERVER['DOCUMENT_ROOT']) . '/../tmp'));
 session_start();
 
 // Check if the user is not logged in, redirect to login page
--- login.php
+++ login.php
@@ -1,5 +1,4 @@
 <?php
-session_save_path(realpath(dirname($_SERVER['DOCUMENT_ROOT']) . '/../tmp'));
 session_start();
 
 // Check if the user is already logged in, if yes, redirect to the main page
