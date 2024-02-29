d29d6e1739b25d85d318f4fcbc094abce519186b
Karsten Dambekalns <karsten@dambekalns.de>
2024-02-29 23:30:03 +0100

    Remove session cookie on logout

--- logout.php
+++ logout.php
@@ -3,5 +3,16 @@ if (session_status() !== PHP_SESSION_ACTIVE) {
     session_start();
 }
 
+// Remove the session cookie
+if (ini_get('session.use_cookies')) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params['path'], $params['domain'],
+        $params['secure'], $params['httponly']
+    );
+}
+
+// Destroy the session data
 session_destroy();
-header("Location: login_page.php");
+
+header('Location: login_page.php');
