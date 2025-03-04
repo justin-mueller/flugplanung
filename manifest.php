22cfc3f2f9aba73ffee2f4257d4f628b90933a6c
Karsten Dambekalns <karsten@dambekalns.de>
2025-03-04 18:46:13 +0100

    Allow use as PWA on iPhone / Android

---
+++ manifest.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace JustinMueller\Flugplanung;
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+Helper::loadConfiguration();
+
+$clubId = Helper::$configuration['clubId'];
+$club = Helper::$configuration['clubs'][$clubId];
+
+$shortName = $club['shortName'];
+$logo = strtolower($shortName);
+
+header('Content-type: application/json');
+
+echo <<<JSON
+{
+    "name": "Flugplanung {$shortName}",
+    "short_name": "{$shortName}",
+    "display": "standalone",
+    "start_url": "/",
+    "icons": [
+      {
+        "src": "/img/clubs/{$logo}.png",
+        "sizes": "192x192",
+        "type": "image/png"
+      }
+    ]
+  }
+JSON;
